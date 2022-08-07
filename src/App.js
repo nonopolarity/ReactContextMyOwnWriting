54194aefa2588de0feb663278ae507ad7bafd16c
Kin <winterheat@gmail.com>
2022-08-07 10:13:13 -0700

    ok

--- src/App.js
+++ src/App.js
@@ -1,20 +1,42 @@
 import logo from "./logo.svg";
 import "./App.css";
 import Container from "./Container";
-import ThemeContext, {themeColors} from "./contexts/ThemeContext";
+import ThemeContext, { themeColors } from "./contexts/ThemeContext";
 import { useState } from "react";
 
+const colors = { fg: "orange", bg: "white" };
+let toggle = true;
+
 function App() {
-  const [toggle, setToggle] = useState(true);
+  // const [toggle, setToggle] = useState(true);
+  const [timeNow, setTimeNow] = useState(new Date());
+
   return (
-    <ThemeContext.Provider value={toggle ? themeColors.light : themeColors.dark}>
+    <ThemeContext.Provider value={colors}>
       <div
         className="App"
         style={{ border: "3px dotted #07f", margin: "18px" }}
       >
+        {" "}
+        <h1>Time is {timeNow.toLocaleTimeString()}</h1>
         <Container />
       </div>
-      <button onClick={() => setToggle(t => !t)}>Toggle colors</button>
+      <button
+        onClick={() => {
+          toggle = !toggle;
+          colors.fg = toggle ? "orange" : "black";
+        }}
+      >
+        Toggle colors
+      </button>
+
+      <button
+        onClick={() => {
+          setTimeNow(new Date());
+        }}
+      >
+        Update time
+      </button>
     </ThemeContext.Provider>
   );
 }
